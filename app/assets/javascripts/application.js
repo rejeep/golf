fbde91a33fe5cf7b5ed3773bfcbc9be7a35f302c
Johan Andersson <johan.rejeep@gmail.com>
2011-11-18 10:36:37 +0100

    Adding some styles.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,13 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require bootstrap/alerts
+//= require bootstrap/dropdown
+//= require bootstrap/modal
+//= require bootstrap/popover
+//= require bootstrap/scrollspy
+//= require bootstrap/tabs
+//= require bootstrap/twipsy
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def page_name
+    @page_name || t('page.name')
+  end
+  
+  def page_description
+    @page_name || t('page.description')
+  end
+  
+  def menu_link(name)
+    path = send("#{name}_path")
+    options = {}
+    options[:class] = 'active' if path == request.path
+    content_tag(:li, options) do
+      link_to t("menu.#{name}"), path
+    end
+  end
 end
