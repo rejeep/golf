2ba4038d13fabaf54a08bef486ef1f6330432d93
Johan Andersson <johan.rejeep@gmail.com>
2011-11-18 09:18:41 +0100

    Added Rails files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Golf::Application.config.secret_token = '6d69de9fb90c92a0a090cb836bd0b75e02ee003856d3c5a9a40dbc234913ee8b4b2e1dc59fdb596ecb4456576d2d0f2974d8e10ac6b36309e63f8250cec12834'
