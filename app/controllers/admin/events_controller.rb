5af3c79c0874fc0dc5414be4725326f783db69dc
Johan Andersson <johan.rejeep@gmail.com>
2011-11-20 17:15:41 +0100

    Use admin layout.

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,2 +1,3 @@
 class Admin::EventsController < InheritedResources::Base
+  layout 'admin'
 end
--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -1,15 +1,17 @@
 class Admin::PhotosController < InheritedResources::Base
+  layout 'admin'
+
   def create
     super(&redirect_to_index)
   end
-  
+
   def update
     super(&redirect_to_index)
   end
-  
-  
+
+
   private
-  
+
   def redirect_to_index
     Proc.new { admin_photos_path }
   end
