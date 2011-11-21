cdf1f8bcce284ba1f2f3563b1648f75e9cc0eae1
Johan Andersson <johan.rejeep@gmail.com>
2011-11-21 20:03:31 +0100

    Validate user first and last name in before filter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   before_filter do
     @page_title = t("page.#{params[:controller]}", :default => t('page.fallback'))
   end
+
+  def validate_user_name
+    if user_signed_in? and not current_user.name?
+      session[:redirect_to] = request.path
+      
+      redirect_to edit_user_path(current_user)
+    end
+  end
+  before_filter :validate_user_name
 end
