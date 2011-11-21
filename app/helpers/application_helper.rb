2b7232b98e69c47f5e9da98097d9318238db1bf9
Johan Andersson <johan.rejeep@gmail.com>
2011-11-21 10:28:00 +0100

    Style some admin parts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,19 +2,37 @@ module ApplicationHelper
   attr_reader :page_name, :page_description
 
   def menu_link(name)
-    path = send("#{name}_path")
-    options = {}
-    options[:class] = 'active' if path == request.path
-    content_tag(:li, options) do
-      link_to t("menu.#{name}"), path
-    end
+    create_menu_link(name)
   end
-  
+
+  def admin_menu_link(name)
+    create_menu_link(name, :admin)
+  end
+
   def sidebar(&block)
     content_for(:sidebar, &block)
   end
-  
+
   def slider(&block)
     content_for(:slider, &block)
+  end
+
+
+  private
+
+  def create_menu_link(name, scope = nil)
+    if scope.present?
+      path = send("admin_#{name}_path")
+      link_name = t("menu.admin.#{name}")
+    else
+      path = send("#{name}_path")
+      link_name = t("menu.#{name}")
+    end
+    
+    options = {}
+    options[:class] = 'active' if path == request.path
+    content_tag(:li, options) do
+      link_to link_name, path
+    end
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ActiveRecord::Base
-  has_many :photos
-  has_many :videos
-  
+  has_many :photos, :dependent => :destroy
+  has_many :videos, :dependent => :destroy
+
   def random_photos
     photos.random(3)
   end
-  
+
   def random_videos
     videos.random(3)
   end
