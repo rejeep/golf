bd45d667d4601c9a4d0affc53930c0b352333bf0
Johan Andersson <johan.rejeep@gmail.com>
2011-11-22 10:54:41 +0100

    Update events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
   has_many :photos, :dependent => :destroy
   has_many :videos, :dependent => :destroy
 
+  validates :name, :start_date, :presence => true
+
   def random_photos
     photos.random(3)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111122085746) do
+ActiveRecord::Schema.define(:version => 20111122095015) do
 
   create_table "blog_posts", :force => true do |t|
     t.string   "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(:version => 20111122085746) do
     t.string   "eventable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "start_date"
+    t.date     "end_date"
   end
 
   create_table "photos", :force => true do |t|
