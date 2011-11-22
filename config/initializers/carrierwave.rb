af1d190313d5a000e88cd1bb49f06f055a4db48a
Johan Andersson <johan.rejeep@gmail.com>
2011-11-22 21:21:37 +0100

    Update carrierwave conf.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,9 @@
- CarrierWave.configure do |config|
-   config.s3_access_key_id = ENV['S3_ACCESS_KEY_ID']
-   config.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
-   config.s3_bucket = 'leadingedge'
-   config.s3_region = 'eu-west-1'
- end
+CarrierWave.configure do |config|
+  config.s3_access_key_id = ENV['S3_ACCESS_KEY_ID']
+  config.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+  config.s3_bucket = 'leadingedge'
+  config.s3_region = 'eu-west-1'
+
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
+end
