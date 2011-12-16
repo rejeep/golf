28389c18d14f3c8aabf9e28a8dd6cdeb2b3cc4ae
Johan Andersson <johan.rejeep@gmail.com>
2011-12-16 16:18:36 +0100

    Use File class to get extname.

--- app/uploaders/random_uploader.rb
+++ app/uploaders/random_uploader.rb
@@ -1,9 +1,11 @@
 class RandomUploader < CarrierWave::Uploader::Base
   def filename
-    "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+    "#{secure_token(10)}.#{File.extname(original_filename)}" if original_filename.present?
   end
+  
 
   protected
+  
   def secure_token(length=16)
     var = :"@#{mounted_as}_secure_token"
     model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
