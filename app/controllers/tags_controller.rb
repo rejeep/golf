d2905e72650c58545527a11904c89444af7e1e6d
Johan Andersson <johan.rejeep@gmail.com>
2011-12-22 20:21:51 +0100

    Case insensitive tag search.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   def index
     respond_to do |format|
       format.json do
-        render :json => ActsAsTaggableOn::Tag.where('name LIKE ?', "%#{params[:q]}%").map do |tag|
+        render :json => ActsAsTaggableOn::Tag.where('LOWER(name) LIKE ?', "%#{params[:q].downcase}%").map do |tag|
           { :id => tag.id, :name => tag.name }
         end
       end
